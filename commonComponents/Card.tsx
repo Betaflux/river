32577b954d8d9c74ba4b0a55885c336ff26c0324
satishkumarojha <satishkumarojha15@gmail.com>
2023-01-24 22:30:25 +0530

    article tag added to the card component

--- commonComponents/Card.tsx
+++ commonComponents/Card.tsx
@@ -1,14 +1,14 @@
-import Image from 'next/image';
-import React from 'react'
- 
-export interface ICard{
-  title:string;
-  paragraph:string;
-  image:string;
+import Image from "next/image";
+import React from "react";
+
+export interface ICard {
+  title: string;
+  paragraph: string;
+  image: string;
 }
-const Card = ({title,paragraph,image}:ICard) => {
+const Card = ({ title, paragraph, image }: ICard) => {
   return (
-    <div className="w-full bg-blue-92 p-5 lg:p-10">
+    <article className="w-ful">
       <div className="w-full">
         <Image
           src={image}
@@ -17,16 +17,16 @@ const Card = ({title,paragraph,image}:ICard) => {
           className="relative w-full h-full"
         />
       </div>
-      <div className="w-full">
-        <div className="text-blue-37 py-2 font-sohneHalbfett  text-10xl leading-11 lg:leading-12 lg:text-12xl">
+      <div className="w-full py-[14px] pl-[20px] pr-[32px] lg:py-[38px] lg:pl-[40px] lg:pr-[64px] bg-blue-92">
+        <div className="text-blue-37  font-sohneHalbfett  text-10xl leading-11 lg:leading-12 lg:text-12xl">
           {title}
         </div>
-        <div className="text-blue-37 font-sohneBuch leading-11 text-10xl lg:text-12xl lg:leading-12">
+        <p className="text-blue-37 font-sohneBuch leading-11 text-10xl lg:text-12xl lg:leading-12">
           {paragraph}
-        </div>
+        </p>
       </div>
-    </div>
+    </article>
   );
-}
+};
 
-export default Card
+export default Card;
