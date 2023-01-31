4c7b1c574851b740e279dfe560ac89639fd8e272
satishkumarojha <satishkumarojha15@gmail.com>
2023-01-31 10:07:19 +0530

    updated env and baseurl

--- http/index.tsx
+++ http/index.tsx
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL
+  baseURL: process.env.NEXT_PUBLIC_STRAPI_URL,
 });
-
 
 // eslint-disable-next-line import/prefer-default-export
 export const getPurposeData = async () => api.get("/purpose?populate=deep");
